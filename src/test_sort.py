113c8d67a9289ca4aeecceb090f59c92b1b12e1c
zwidny <649038269@qq.com>
2015-09-23 23:24:44 +0800

    Add a stack example

--- src/test_sort.py
+++ src/test_sort.py
@@ -1,8 +1,19 @@
-from zy_sort import quick_sort
+from zy_sort import quick_sort, ListStack
 import unittest
 
 
 class TestSort(unittest.TestCase):
+
+    def test_liststack(self):
+        s = ListStack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        self.assertEqual(3, s.pop())
+        self.assertEqual(2, len(s))
+        self.assertEqual(2, s.pop())
+        self.assertEqual(1, s.pop())
+        self.assertEqual(True, s.is_empty())
 
     def test_quick_sort(self):
         s = [24]
--- src/zy_sort.py
+++ src/zy_sort.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 
 
+class ListStack:
+    """LIFO Stack implementation using list
+    """
+
+    def __init__(self):
+        self.data_repo = []
+
+    def __len__(self):
+        return len(self.data_repo)
+
+    def is_empty(self):
+        return len(self.data_repo) == 0
+
+    def push(self, element):
+        self.data_repo.append(element)
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        return self.data_repo.pop()
+
+
 def quick_sort(s):
     """Quick sort.
     Args:
@@ -14,8 +36,8 @@ def quick_sort(s):
     if len(s) < 2:
         return s
     p = s.pop()
-    pl = type(s)(filter(lambda x: x < p, s))
-    pg = type(s)(filter(lambda x: x > p, s))
+    pl = list(filter(lambda x: x < p, s))
+    pg = list(filter(lambda x: x > p, s))
     pl = quick_sort(pl)
     pg = quick_sort(pg)
     return pl + [p] + pg
